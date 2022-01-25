886ded9aab3f48988aef29ec5602f3e15612eeb9
CosmoAndrade <cosmo.andrade@hotmail.com>
2022-01-24 23:02:56 -0300

    Parte 06

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
-const form = document.getAnimations('form');
-
+const form = document.getElementById('form');
+form.addEventListener('submit', handleSubmit);
 
 const inputValue = document.getElementById('value-real');
-const selectCurrency = document.getElementById('currency');
+const selectedCurrency = document.getElementById('currency');
 const result = document.getElementById('result');
+let valueConverted = 0;
 
-form.addEventListener('submit',handleSubmit);
+function handleSubmit(e) {
+  e.preventDefault();
 
+  if(!inputValue.value || inputValue.value < 0) {
+    alert('Informe um valor correto!');
+    return;
+  } else if(!selectedCurrency.value) {
+    alert('Escolha uma moeda!');
+    return;
+  }
 
-function handleSubmit(e){
-    e.preventDefault();
+  converter();
+};
 
-    if(!inputValue.value || inputValue.value < 0){
-        alert("Informe um valor correto!");
-        return;
-    }else if (!selectCurrency.value){
-        alert("Escolha uma moeda!")
-    }
-}
+function converter() {
+  if(selectedCurrency.value === 'eur') {
+    valueConverted = inputValue.value * 6.35;
+    result.innerHTML = valueFormatter('pt-PT', 'EUR');
+
+    animateResult();
+  } else if(selectedCurrency.value === 'dol') {
+    valueConverted = inputValue.value * 5.61;
+    result.innerHTML = valueFormatter('en-US', 'USD');
+
+    animateResult();
+  }
+
+  inputValue.value = '';
+  selectedCurrency.value = '';
+};
+
+function valueFormatter(locale, currency) {
+  const value = valueConverted.toLocaleString(`${locale}`, { style: 'currency', currency: `${currency}` });
+  return `<span>🤑</span> ${value} <span>🤑</span>`;
+};
+
+function animateResult() {
+  return result.animate([
+    { transform: 'translateY(-150px)'},
+    { transform: 'translateY(0px)'}
+  ], { duration: 500 });
+};
